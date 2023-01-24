a02a5984573b0f30b4bec02867368a758bb4488a
renata143 <renata.araujo@ufvjm.edu.br>
2023-01-24 15:59:18 -0300

    criação de formulário

---
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarefas.Web.Controllers;
+
+public class TarefasController : Controller 
+{
+    public IActionResult Create()
+    {
+        return View ();
+    }
+
+}
