e617b79f8ef9abe73fab106bd9cf48e04082ae82
TiagoMerc <tmrrosario9@gmail.com>
2023-01-25 13:49:01 -0300

    Criação de lista

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
@@ -10,4 +11,14 @@ public class TarefasController : Controller
         return View ();
     }
 
+    public IActionResult Index()
+    {
+        var listaDeTarefas = new List <TarefaViewModel>()
+        {
+            new TarefaViewModel() {Titulo = "Escovar os dentes", Descricao = "Usar sensodine de manhã e não a noite"},
+            new TarefaViewModel() {Titulo = "Arrumar a cama"},
+            new TarefaViewModel() {Titulo = "Tirar o lixo", Descricao = "seg, quar, sex"},
+        };
+        return View(listaDeTarefas);
+    }
 }
